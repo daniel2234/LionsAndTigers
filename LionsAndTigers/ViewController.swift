046a82e21a50ea8f3fa54d7ebb86787ba01a4c41
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-02-04 21:41:34 -0500

    added OOP practices

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -32,6 +32,24 @@ class ViewController: UIViewController {
         ageLabel.text = "\(myTiger.age)"
         breedLabel.text = myTiger.breed
         
+        //made 3 instances of of different tigers, also to further my undertanding of encapsulation to understanding object-oriented programming
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named:"IndochineseTiger.jpg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Jacob"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.age = 4
+        thirdTiger.image = UIImage(named:"MalayanTiger.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Spar"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 5
+        fourthTiger.image = UIImage(named:"SiberianTiger.jpg")
         
     }
 
