c006c1eb7b84e9c4dc0c1be8bf9d7678605d682d
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-02-04 20:07:15 -0500

    introduction to structs and instances

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Daniel Kwiatkowski on 2015-02-04.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+import Foundation
+import UIKit //creating a struct for a tiger
+
+struct Tiger{
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myTiger = Tiger()
+        myTiger.name = "Tigger" //creating and instance for the struct(blueprint)
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        println("My Tiger's name is: \(myTiger.name), its' age is \(myTiger.age), its' breed is \(myTiger.breed) and its' image is \(myTiger.image)")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +36,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+    }
 }
 
